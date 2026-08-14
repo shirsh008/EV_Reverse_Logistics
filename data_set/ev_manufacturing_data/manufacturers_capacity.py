1f2f64a02f7c40180816e48efd88ab2896a6300f
Shirsh Agarwal <shirsh.agarwal.05@gmail.com>
2026-08-14 19:29:26 +0530

    Create manufacturers_capacity.py

---
+++ data_set/ev_manufacturing_data/manufacturers_capacity.py
@@ -0,0 +1,30 @@
+manufacturers_capacity = ('''
+240 MWh
+120000 packs
+1000 MWh
+20 GWh
+12.5 GWh
+5 GWh
+1500 mwh
+10 GWh
+20 GWh
+500 mwh
+5 GWh
+288 MWh
+400 MWh
+50000 packs
+1 GWh
+155000 packs
+100 MWh
+200 MWh
+1500 MWh
+400 MWh
+200 MWh
+140 MWh
+400 MWh
+500 MWh
+250 MWh
+750 MWh
+100 MWh
+350 MWh
+''')
