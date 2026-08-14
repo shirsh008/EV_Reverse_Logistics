02732c00151a13196deac18c972e3a16ae7ba82d
Shirsh Agarwal <shirsh.agarwal.05@gmail.com>
2026-08-14 19:21:52 +0530

    Create manufacturers.py

---
+++ data_set/ev_manufacturing_data/manufacturers.py
@@ -0,0 +1,30 @@
+ev_manufacturers = ('''
+coslight india
+ruchira green earth
+power global india
+agartas
+godi india hyderabad
+jsw energy
+li energy
+excide industries
+ola electric 
+Q max
+Rajesh exports
+okaya
+livgaurd
+future punjab
+exicom
+ipower
+greenfuel
+lohum
+nexcharge
+waaree
+renon
+forsee
+grinntech
+amara batteries telangana
+pure
+cygni energy
+inverted
+trontek
+''')
