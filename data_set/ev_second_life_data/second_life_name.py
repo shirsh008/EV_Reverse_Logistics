12005b5063431a2846c9b1825892b6f66bf67621
Shirsh Agarwal <shirsh.agarwal.05@gmail.com>
2026-08-28 19:25:55 +0530

    Update second_life_name.py

--- data_set/ev_second_life_data/second_life_name.py
+++ data_set/ev_second_life_data/second_life_name.py
@@ -1,25 +1,25 @@
 second_life_name = ('''
-tata power renewables
-tata power renewables
-tata power renewables
-tata power renewables
-avaada
-avaada
+tata power renewables 1
+tata power renewables 2
+tata power renewables 3
+tata power renewables 4
+avaada 1
+avaada 2
 adani
-jsw
-jsw
-ntpc
-ntpc
-ntpc
+jsw 1
+jsw 2
+ntpc 1
+ntpc 2
+ntpc 3
 reliance
-acme
-acme
+acme 1
+acme 2
 hero futures
-amara raja
-amara raja
+amara raja 1
+amara raja 2
 exide
-luminous
-luminous
-luminous
+luminous 1
+luminous 2
+luminous 3
 ''')
 ev_second_life_name = second_life_name.strip().split('\n')
