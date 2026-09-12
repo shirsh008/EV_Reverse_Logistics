62b8115b50061ca8bc126ec1d576678b664dcde2
Shirsh Agarwal <shirsh.agarwal.05@gmail.com>
2026-09-13 01:57:53 +0530

    Create refurbisher_longitude.py

---
+++ data_set/ev_2050_data/refurbisher/refurbisher_longitude.py
@@ -0,0 +1,7 @@
+refurbisher_longitude = ('''
+77.59370913
+75.71984142
+75.66903367
+''')
+refurbisher_longitude = refurbisher_longitude.strip().split('\n')
+ev_refurbisher_longitude = [float(x) for x in refurbisher_longitude]
