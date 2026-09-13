e8decba6cc26122b263eb655d6ba5a28d705c4de
Shirsh Agarwal <shirsh.agarwal.05@gmail.com>
2026-09-13 14:09:32 +0530

    Create black_mass_latitude.py

---
+++ data_set/ev_2040_data/black_mass/black_mass_latitude.py
@@ -0,0 +1,10 @@
+black_mass_latitude = ('''
+21.17815544
+28.79350456
+28.48146333
+12.95177349
+11.69021496
+23.69474218
+''')
+black_mass_latitude = black_mass_latitude.strip().split('\n')
+ev_black_mass_latitude = [float(x) for x in black_mass_latitude]
