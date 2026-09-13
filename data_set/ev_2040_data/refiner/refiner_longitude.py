0d370dcd851156f808ca16322aeb77aea4cb8cd4
Shirsh Agarwal <shirsh.agarwal.05@gmail.com>
2026-09-13 14:21:43 +0530

    Create refiner_longitude.py

---
+++ data_set/ev_2040_data/refiner/refiner_longitude.py
@@ -0,0 +1,7 @@
+refiner_longitude = ('''
+77.59370913
+79.08503025
+77.10773924
+''')
+refiner_longitude = refiner_longitude.strip().split('\n')
+ev_refiner_longitude = [float(x) for x in refiner_longitude]
