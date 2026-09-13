11fc27ef5eb97ee51fdbb05f88d2b0245f2da3fd
Shirsh Agarwal <shirsh.agarwal.05@gmail.com>
2026-09-13 12:22:15 +0530

    Create refurbisher_latitude.py

---
+++ data_set/ev_2045_data/refurbisher/refurbisher_latitude.py
@@ -0,0 +1,8 @@
+refurbisher_latitude = ('''
+23.47015891
+20.02744194
+21.17815544
+23.33088309
+''')
+refurbisher_latitude = refurbisher_latitude.strip().split('\n')
+ev_refurbisher_latitude = [float(x) for x in refurbisher_latitude]
