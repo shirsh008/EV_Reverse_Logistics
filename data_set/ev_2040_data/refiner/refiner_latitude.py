2f2a57bfef5470ff566f5c5c0a8296243ecea2f4
Shirsh Agarwal <shirsh.agarwal.05@gmail.com>
2026-09-13 14:20:57 +0530

    Create refiner_latitude.py

---
+++ data_set/ev_2040_data/refiner/refiner_latitude.py
@@ -0,0 +1,7 @@
+refiner_latitude = ('''
+12.95177349
+21.17815544
+28.79350456
+''')
+refiner_latitude = refiner_latitude.strip().split('\n')
+ev_refiner_latitude = [float(x) for x in refiner_latitude]
